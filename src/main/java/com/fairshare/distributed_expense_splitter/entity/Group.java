3d55886414a45e10388fa602ba97fbd2e98302e3
Pnilay <pnilay926@gmail.com>
2026-06-05 22:45:28 -0400

    initialize core database entities

---
+++ src/main/java/com/fairshare/distributed_expense_splitter/entity/Group.java
@@ -0,0 +1,32 @@
+package com.fairshare.distributed_expense_splitter.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "groups")
+public class Group {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column
+  private String description;
+
+}
